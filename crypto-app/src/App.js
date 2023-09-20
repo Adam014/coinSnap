0ec8b937962e50896f317895a5b927cf20dcee59
Adam Stádník <adam.stadnik@spsrakovnik.onmicrosoft.com>
2023-09-20 20:56:36 +0200

    Added navbar, updated Sass

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,9 +1,32 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import {Layout, Typography, Space } from 'antd';
+import './App.sass';
+
+import { Navbar, Exchanges, Homepage, CryptoDetails, Cryptocurrencies, News } from './components';
 
 export default function App() {
   return (
     <div className="App">
+        <div className='navbar'>
+            <Navbar />
+        </div>
+        <div className='main'>
+            <Layout>
+                <div className='routes'>
+                  <Routes>
+                    <Route exact path="/" element={<Homepage />} />
+                    <Route exact path="/exchanges" element={<Exchanges />} />
+                    <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />}/>
+                    <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+                    <Route exact path="/news" element={<News />} />
+                  </Routes>
+                </div>
+            </Layout>
+        </div>
+        <div className='footer'>
 
+        </div>
     </div>
-  );
+  )
 }
---
+++ crypto-app/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+import cryptoLogo from '../images/cryptoLogo.png';
+
+export default function Navbar(){
+    return ( 
+        <div className="nav-container">
+            <div className="logo-container">
+                <Avatar src={cryptoLogo} size="large"/>
+                <Typography.Title level={2} className="logo">
+                    <Link to="/">CryptoApp</Link>
+                </Typography.Title>
+            </div>
+            <div className='links-container'>
+                <Menu theme="dark">
+                    <Menu.Item icon={<HomeOutlined/>}>
+                        <Link to="/">Home</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<FundOutlined/>}>
+                        <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<MoneyCollectOutlined/>}>
+                        <Link to="/exchanges">Exchanges</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<BulbOutlined/>}>
+                        <Link to="/news">News</Link>
+                    </Menu.Item>
+                </Menu>
+            </div>
+        </div>
+    )
+}
