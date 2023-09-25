9a1b2348dea9016446796dd0e3652e642bfc72b0
Adam Stádník <adam.stadnik@spsrakovnik.onmicrosoft.com>
2023-09-25 21:34:18 +0200

    Added the chart, but it shows bad timestamps

--- crypto-app/src/components/index.js
+++ crypto-app/src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Navbar } from "./Navbar";
-export { default as Exchanges } from "./Exchanges";
 export { default as Cryptocurrencies } from "./Cryptocurrencies";
 export { default as CryptoDetails } from "./CryptoDetails";
 export { default as Homepage} from "./Homepage";
--- crypto-app/src/services/cryptoApi.js
+++ crypto-app/src/services/cryptoApi.js
@@ -21,11 +21,16 @@ export const cryptoApi = createApi({
         }),
         getCryptoDetails: builder.query({
             query: (coinId) => createRequest(`/coin/${coinId}`),
-        })
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) =>
+                createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+        }),
     })
 })
 
 export const {
     useGetCryptoQuery,  
-    useGetCryptoDetailsQuery
+    useGetCryptoDetailsQuery,
+    useGetCryptoHistoryQuery
 } = cryptoApi
