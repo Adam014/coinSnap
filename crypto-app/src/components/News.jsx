266522618389f382ea59cd4052a03083b51fcae3
Adam Stádník <adam.stadnik@seznam.cz>
2023-11-26 17:28:38 +0100

    Refactored code, bcs API from Microsoft stopped working

--- crypto-app/src/components/News.jsx
+++ crypto-app/src/components/News.jsx
@@ -10,28 +10,27 @@ const { Option } = Select;
 const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News'
 
 const News = ( { simplified } ) => {
-  const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ count: simplified ? 6 : 12 });
 
   console.log(cryptoNews);
 
-  if(!cryptoNews?.value) return 'Loading...)';
+  if(!cryptoNews) return 'Loading...)';
 
   return (
     <Row gutter={[24, 24]}> 
-      {!simplified && (
+      {/* {!simplified && (
           <Col>
             <Select showSearch className='select-news' placeholder="Select a Crypto" optionFilterProp='children' onChange={(value) => console.log(value)} filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}> 
 
             </Select>
           </Col>
-      )}
-      {cryptoNews.value.map((news, i) => (
+      )} */}
+      {cryptoNews.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className='news-card'>
               <a href={news.url} target="_blank" rel="noreferrer">
                 <div className='news-image-container'>
-                    <Title className='news-title' level={4}>{news.name}</Title>
+                    <Title className='news-title' level={4}>{news.title}</Title>
                     <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="crypto-news-thumbnail"/>
                 </div>
                 <p>
@@ -40,13 +39,13 @@ const News = ( { simplified } ) => {
                       : news.description
                     } 
                 </p>
-                <div className='provider-container'>
+                {/* <div className='provider-container'>
                     <div>
                       <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news"/>
                       <Text className='provider-name'>{news.provider[0]?.name}</Text>
                     </div>
                     <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
-                </div>
+                </div> */}
               </a>
           </Card>
         </Col>
--- crypto-app/src/services/cryptoNewsApi.js
+++ crypto-app/src/services/cryptoNewsApi.js
@@ -2,20 +2,21 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeaders = {
     'X-BingApis-SDK': 'true',
-    'X-RapidAPI-Key': '8f9955e271mshd99db096c9540ccp164f0cjsn936ce88b5d7e',
-    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+    'X-RapidAPI-Key': 'c605a3131fmshfefeff37100a42ap1afec7jsn49bf9d6da20e',
+    'X-RapidAPI-Host': 'crypto-news16.p.rapidapi.com'
 };
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
-const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const baseUrl = 'https://crypto-news16.p.rapidapi.com';
 
 export const cryptoNewsApi = createApi({
   reducerPath: 'cryptoNewsApi',
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
-      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+      query: ({ count }) => createRequest(`/news/top/${count}`),
     }),
   }),
 });
